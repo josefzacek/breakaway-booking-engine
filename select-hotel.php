50ab19c38201336489e0d222653e895964f0d500
Josef Zacek <info@josefzacek.com>
2013-11-11 13:05:51 +0000

    select hotel desktop view updated to have hotel option and hotel detail next to each other

--- select-hotel.php
+++ select-hotel.php
@@ -223,6 +223,10 @@
     	<div id="div_description_0" class="content" style="display:none">
             <img src="images/select-hotel/36_2_STD.jpg" alt="Santa Fe " />
             <p class="general">Start the day with a hearty continental breakfast. Later, tuck in at "La Cantina", the Mexican themed buffet restaurant. Enjoy musical entertainment over a cocktail at the "Rio Grande Bar"."Pow Wow Game Arcade" video games room, indoor children's corners (under parental surveillance), souvenir shop, TV and games area. "Totem Circle" outdoor playground. After a fun packed day at the parks, you want to relax, but the kids want to play on. No problem, compadre!</p>
+            
+<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
     	</div>	
         
         
@@ -244,6 +248,9 @@
         	<p class="general"> Continue the Navajo theme with cheerfully decorated rooms equipped with two double beds, including bathroom, telephone, TV with Disney and international channels, radio and a ceiling fan.</p>
             <p><b>Exclusive Rio Grande & Eldorado rooms</b></p>
             <p>Choose a room along the Rio Grande for more calm and direct access to the path leading to the Disney Parks.<br/>An Eldorado room is ideally located near all the main hotel facilities: reception, restaurant, bar and Disney shop.</p>
+            <p>Contrary to
+
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
     	</div>	<!-- div_facilities end -->
             
             
