e19708e81eb46a1da8347aff4a52b8f71a79e69c
Josef Zacek <info@josefzacek.com>
2013-11-11 16:16:43 +0000

    deleted original shared

--- shared/header.php
+++
@@ -1,5 +0,0 @@
-<header>
-  <a href="http://www.breakaway.ie" title="Breakaway.ie"><img src="images/breakawaylogo.gif" alt="Breakaway logo" id="breakaway-logo"/></a>
-  <img src="images/clicks-away.gif" alt="Breakaway 01 6079900" id="header-image"/>
-  <div class="clear-both"></div>
-</header>
