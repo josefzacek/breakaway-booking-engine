854491a1a5cb14e03426d94a81cc0305a56cc166
Josef Zacek <info@josefzacek.com>
2013-11-12 12:10:14 +0000

    my booking page created and finished

---
+++ my-booking.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>My booking</title>
+<link rel="stylesheet" type="text/css" href="css/shared-styles.css"/>
+<link rel="stylesheet" type="text/css" href="css/my-booking.css"/>
+<!--[if lt IE 9]>
+	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+</head>
+
+<body>
+<div id="wrapper" class="my-booking">
+	<h2 class="center">My booking - search</h2>
+    
+    <div id="my-booking-form">
+    <form action="#" method="post" class="center">
+		<p>
+          <label>Reference number:<br />
+            <input type="text" name="referenceNumber" required>
+          </label>
+        </p>
+		<p>
+          <label>Password:<br />
+          	<input type="password" name="password" required>
+          </label>
+        </p>
+        <p><input type="submit" value="Next"></p>
+	</form>
+    </div> <!-- my-booking-form end -->
+
+</div> <!-- wrapper end -->
+
+</body>
+</html>
