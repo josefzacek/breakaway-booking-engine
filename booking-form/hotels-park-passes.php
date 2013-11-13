8399744e9c4b0878477e1a9eae3d30d0e4c24083
Josef Zacek <info@josefzacek.com>
2013-11-13 15:58:40 +0000

    booking form  basic started

--- booking-form/hotels-park-passes.php
+++ booking-form/hotels-park-passes.php
@@ -14,19 +14,100 @@
 	<div id="booking-form-middle-box-holder">
       <nav>
           <ul>
-              <li><a href="hotels-park-passes.php">Hotels &amp; Park Passes</a></li>
+              <li class="current"><a href="hotels-park-passes.php">Hotels &amp; Park Passes</a></li>
               <li><a href="park-pass-only.php">Park Pass Only</a></li>
               <li><a href="special-offers.php">Special Offers</a></li>
           </ul>
       </nav>
-      <div id="booking-form">
-        <p>hotels and park passes</p>
-        <p>hotels and park passes</p>
-        <p>hotels and park passes</p>
-        <p>hotels and park passes</p>
-        <p>hotels and park passes</p>
-        <p>hotels and park passes</p>
+    
+        <form action="#" method="post">
+          <div id="booking-form">
+          <p>
+        	<label>
+            	Departure date:<br/>
+                <input type="date">
+            </label>
+        </p>
+        <p>
+        	<label>
+            	Return date:<br/>
+                <input type="date">
+            </label>
+        </p>
+        <p>
+        	<label>
+            	Number of Adults:<br/>
+                	<select>
+					  <option value="1">1</option>
+					  <option value="2">2</option>
+					  <option value="3">3</option>
+					  <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+					</select>
+			</label>
+        </p>
+           <p>
+        	<label>
+            	<sup>*</sup>Number of children aged 7-11 yrs:<br/> 
+                	<select>
+                      <option value="0">0</option>
+					  <option value="1">1</option>
+					  <option value="2">2</option>
+					  <option value="3">3</option>
+					  <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+					</select>
+			</label>
+        </p>
+             <p>
+        	<label>
+            	<sup>*</sup>Number of children agen 3-6 yrs:<br/>
+                	<select>
+                      <option value="0">0</option>
+					  <option value="1">1</option>
+					  <option value="2">2</option>
+					  <option value="3">3</option>
+					  <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+					</select>
+			</label>
+        </p>
+             <p>
+        	<label>
+            	<sup>*</sup>Number of children agen 0-2 yrs: <br/>
+                	<select>
+                      <option value="0">0</option>
+					  <option value="1">1</option>
+					  <option value="2">2</option>
+					  <option value="3">3</option>
+					  <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+					</select>
+			</label>
+        </p>
+    
+        
+        <p><sup>*</sup>Age on date of arrival, not date of booking</p>
       </div> <!-- booking-form end -->
+      <input type="button" value="Search" />
+      </form>
+      <p class="center">Want to view an existing booking? <a href="../my-booking.php">Click here</a></p>
 	</div> <!-- booking-form-middle-box end -->
 </body>
 </html>
