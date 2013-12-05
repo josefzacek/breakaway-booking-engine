62e65b06f599a2ee303dce9936749e3d36f5a162
Josef Zacek <info@josefzacek.com>
2013-12-05 15:53:30 +0000

    added final-booking-details to booking page - optimized for mobile

--- booking-details.php
+++ booking-details.php
@@ -34,10 +34,90 @@
   <!-- navigation end -->
 
   <div class="spacer"></div>
+  
+  <div class="final-booking-details">
+  <h3>Booking details</h3>
+  <table>
+  	<tr>
+  		<td>Arrival Date</td>
+  		<td>26/12/2013</td>
+  	</tr>
+  	<tr>
+  		<td>Departure Date</td>
+  		<td>28/12/2013</td>
+  	</tr>
+    
+  	<tr>
+  		<td>No of Nights</td>
+  		<td>2</td>
+  	</tr>
+  	<tr>
+  		<td>No of Adults</td>
+  		<td>1</td>
+  	</tr>
+    <tr>
+  		<td>No of Children</td>
+  		<td>0</td>
+  	</tr>
+    <tr>
+  		<td>No of Infant</td>
+  		<td>0</td>
+  	</tr>
+    
+    <tr>
+  		<td>Hotel Name</td>
+  		<td>Sequoia Lodge</td>
+  	</tr>
+    <tr>
+  		<td>Room Type</td>
+  		<td>Montana Lake Max 4 </td>
+  	</tr>
+    <tr>
+  		<td>No of Rooms</td>
+  		<td>1</td>
+  	</tr>
+    
+    <tr>
+  		<td>Transfer Cost</td>
+  		<td>&euro;40</td>
+  	</tr>
+  	<tr>
+  		<td>Package Cost</td>
+  		<td>&euro;1122</td>
+  	</tr> 
+  	<tr>
+  		<td>Handling Fee</td>
+  		<td>&euro;19</td>
+  	</tr>
+  	<tr>
+  		<td>Tax</td>
+  		<td>&euro;0</td>
+  	</tr>
+  
+  </table>
+  <table class="">
+  <tr>
+  		<td><b>Total Cost</b></td>
+  		<td><b>&euro;1122</b></td>
+  	</tr>
+  </table>
+  </div> <!-- final-booking-details end -->
  
  
  
- <h1 class="booo">booking details page </h1>
+ <p>To complete your booking please enter details below. All fields marked with* must be completed.
+If you wish to save the quote details and return to complete the booking later then simply enter your email address and phone number and go directly to the Save quote for Later button. Your quote option will be held until 12pm (mid-day GMT) tomorrow.</p>
+
+<div class="img-holder">
+  <img src="images/booking-details/realex.png" alt="Realex">
+  <img src="images/booking-details/fully-bonded-licensed.png" alt="Licence">
+</div> <!-- img-holder end -->
+
+
+<div class="payment-details">
+
+payment-details
+</div> <!-- payment-details end -->
 
 
 
