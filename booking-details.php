976fb30a01e208b70cdd8fc264b6005d935932b1
Josef Zacek <info@josefzacek.com>
2013-12-05 13:03:52 +0000

    booking page skeleton done

--- booking-details.php
+++ booking-details.php
@@ -2,15 +2,59 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>booking details</title>
+<title>transfers and insurance</title>
 <link rel="stylesheet" type="text/css" href="css/shared-styles.css"/>
+<link rel="stylesheet" type="text/css" href="css/booking-details.css"/>
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 
 <!--[if lt IE 9]>
 	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]--> 
+<![endif]-->
+
+
+
 </head>
 
 <body>
-booking-details
+<div id="wrapper">
+  <?php include("shared-html/header.php"); ?>
+  <div id="navigation">
+    <div id="start-again-back">
+      <ul>
+        <li><a href="booking-form/hotels-park-passes.php" id="start-again-bt" title="Start again">start again</a></li>
+        <li><a href="select-extras.php" id="back-bt" title="Back">Back</a></li>
+      </ul>
+    </div>
+    <!-- start-again-back end -->
+    
+    <div id="progress"> <img src="images/booking-details/progress4.png" alt="Booking progress"/> </div>
+    <!-- progress end -->
+    <div class="clear-both"></div>
+  </div>
+  <!-- navigation end -->
+
+  <div class="spacer"></div>
+ 
+ 
+ 
+ <h1 class="booo">booking details page </h1>
+
+
+
+
+
+  
+  <div id="bottom-start-again-back">
+    <p><a href="select-extras.php" title="Back">Back</a></p>
+    <p><a href="booking-form/hotels-park-passes.php" title="Start again">Start again</a></p>
+  </div>
+  <!-- bottom-start-again-back end -->
+  
+  <?php include("shared-html/footer.php"); ?>
+</div>
+
+
+
 </body>
+
 </html>
