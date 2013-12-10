a64b89e6905060f1d7b22eceabb4430b098abd3a
Josef Zacek <info@josefzacek.com>
2013-12-10 12:43:29 +0000

    renamed list.php to index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<ul>
+  <li><a href="my-booking.php">My booking</a></li>
+  <li><a href="booking-form/hotels-park-passes.php">Booking form (hotels &amp; park passes, park passes only, special offers)</a></li>
+  <li><a href="please-wait.php">Please wait (searching)</a></li>
+  <li><a href="select-hotel.php">Select Hotel</a></li>
+  <li><a href="select-extras.php">Select Extras</a></li>
+  <li><a href="transfers-insurance.php">Transfers and Insurance</a></li>
+  <li><a href="booking-details.php">Booking details</a></li>
+  
+</ul>
+</body>
+</html>
